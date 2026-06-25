b2119854f31b652f3e1c569978a7a67d3df7888c
Nilton Duarte <nilton.guedes_duarte@digits.schwarz>
2026-06-25 23:04:40 +0200

    logger fix

--- src/core/logger.py
+++ src/core/logger.py
@@ -19,7 +19,7 @@ def setup_logging() -> None:
             structlog.processors.TimeStamper(fmt="iso"),
             websocket_broadcaster,
             # Explicitly force ConsoleRenderer to use our safe_stream object instead of sys.stdout
-            structlog.dev.ConsoleRenderer(colors=is_a_tty, stream=safe_stream),
+            structlog.dev.ConsoleRenderer(colors=is_a_tty),
         ],
         logger_factory=structlog.PrintLoggerFactory(file=safe_stream),
         cache_logger_on_first_use=True,
