d5cf915fa87f5a743ee6ece8142b020107554e36
Nilton Duarte <nilton.guedes_duarte@digits.schwarz>
2026-06-26 20:53:31 +0200

    another try on windows

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
 import sys
 import os
+import multiprocessing
 
-# Intercept None streams for PyInstaller windowed mode
+# 1. Provide multiprocessing support for PyInstaller executables
+if sys.platform.startswith("win"):
+    multiprocessing.freeze_support()
+
+# 2. Safely mock all standard streams for Uvicorn in windowed mode
 if sys.stdout is None:
     sys.stdout = open(os.devnull, "w")
 if sys.stderr is None:
     sys.stderr = open(os.devnull, "w")
+if sys.stdin is None:
+    sys.stdin = open(os.devnull, "r")
 
 from src.desktop_launcher import start_desktop_app
 
