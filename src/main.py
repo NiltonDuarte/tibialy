4045ceff338df9a5f03d47de2f2b765c33b88d97
Nilton Duarte <nilton.guedes_duarte@digits.schwarz>
2026-06-26 20:20:35 +0200

    redirect stdout stderr to null if not exist

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+# Intercept None streams for PyInstaller windowed mode
+if sys.stdout is None:
+    sys.stdout = open(os.devnull, "w")
+if sys.stderr is None:
+    sys.stderr = open(os.devnull, "w")
+
 from src.desktop_launcher import start_desktop_app
 
 if __name__ == "__main__":
